00e985267dd52ba115c2a20db98bac1217ca0dce
danez13 <hernandezdaniel170@gmail.com>
2024-10-07 14:13:10 -0400

    UPDATE DatabaseSchema.sql

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Building(
     BuildingID varchar(),
-    Name varchar(),
+    Name varchar(20),
     CampusName varchar(),
     PRIMARY KEY(BuildingID));
 
 CREATE TABLE Technician(
-    TechnicianID numeric(),
-    Name varchar(),
+    TechnicianID numeric(5),
+    Name varchar(20),
     OfficeNumber Varchar(),
     PhoneNumber varchar(),
     Email varchar(),
@@ -15,11 +15,25 @@ CREATE TABLE Technician(
 CREATE TABLE Classroom(
     RoomID Varchar(), 
     BuidlingID varchar(),
-    RoomType char(),
-    Capacity numeric(),
-    NumPCs numeric(),
-    NumProjectors int(),
-    TechnicianID numeric(),
+    RoomType char(1),
+    Capacity numeric(2),
+    NumPCs numeric(2),
+    NumProjectors int(2),
+    TechnicianID numeric(1),
     PRIMARY KEY(RoomID),
     FOREIGN KEY(BuildingID) references Building,
     FOREIGN KEY(TechnicianID) references Technician);
+
+CREATE TABLE Instructor(
+    InstructorID varchar(),
+    Name varchar(),
+    OfficeNumber varchar(),
+    Email varchar(),
+    PRIMARY KEY(InstructorID));
+
+CREATE TABLE Course(
+    CourseID varchar(),
+    Year numeric(),
+    Semester varchar(),
+    Section varchar()
+    PRIMARY KEY(CourseID));
