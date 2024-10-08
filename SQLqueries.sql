348553a630073953d7820f60b7a7660f2d225957
Dayber Alfonso <0036161sm@gmail.com>
2024-10-08 16:39:10 -0400

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1,6 +1,6 @@
 --Query 1: Display in-progress tickets for a specific date, ordered by time
 SELECT TicketID, IncidentTime, RoomID, InstructorID, TechnicianID, IssueType, Description, Status
 FROM Ticket
-WHERE IncidentDate = '2024-10-01' AND Status = 'In-Progress'
+WHERE IncidentDate = '2024-10-08' AND Status = 'In-Progress'
 ORDER BY IncidentTime;
 
