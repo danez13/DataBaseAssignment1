5eb074c2835b332654099cd55b3e406dcc2da074
Dayber Alfonso <0036161sm@gmail.com>
2024-10-11 17:47:16 -0400

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -4,3 +4,8 @@ FROM Ticket
 WHERE IncidentDate = '2024-10-08' AND Status = 'In-Progress'
 ORDER BY IncidentTime;
 
+--Query 2: List all available slots for Owa Ehan C105
+SELECT SlotTime 
+FROM ClassroomSchedule
+WHERE RoomID = 'C105' AND SlotDate BETWEEN '2024-10-01' AND '2024-10-07'
+  AND CourseID IS NULL;
