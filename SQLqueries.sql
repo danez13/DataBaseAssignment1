7d8fb90bfbd78788df1ad86b7392de0d3de2fd5e
Dayber Alfonso <0036161sm@gmail.com>
2024-10-15 15:38:14 -0400

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -9,3 +9,21 @@ SELECT SlotTime
 FROM ClassroomSchedule
 WHERE RoomID = 'C105' AND SlotDate BETWEEN '2024-10-01' AND '2024-10-07'
   AND CourseID IS NULL;
+
+--Query 3: Timetable for Viertes Haus L131 on Mondays
+SELECT 
+  ClassroomSchedule.SlotTime AS StartTime,
+  TIME_ADD(ClassroomSchedule.SlotTime, INTERVAL 1 HOUR) AS EndTime,
+  Course.Code AS CourseName,
+  Instructor.Name AS InstructorName
+FROM 
+  ClassroomSchedule
+JOIN Course ON ClassroomSchedule.CourseID = Course.CourseID
+JOIN InstructorCourse ON Course.CourseID = InstructorCourse.CourseID
+JOIN Instructor ON InstructorCourse.InstructorID = Instructor.InstructorID
+WHERE 
+  ClassroomSchedule.RoomID = 'L131' 
+  AND DAYOFWEEK(ClassroomSchedule.SlotDate) = 2 -- Monday
+  AND ClassroomSchedule.CourseID IS NOT NULL
+ORDER BY ClassroomSchedule.SlotTime;
+
